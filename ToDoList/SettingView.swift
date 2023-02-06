8c353d9aaddda40c0dbfa7999c053ea1acd36ee0
Tedtya Rady <tedtyarady@gmail.com>
2023-02-06 20:49:25 +1100

    [Modified]: - SettingView.swift
    [Action]:
    - Sign out

--- ToDoList/SettingView.swift
+++ ToDoList/SettingView.swift
@@ -78,7 +78,8 @@ struct SettingView: View {
                 Spacer()
                 
                 Button {
-                    print("Sign Out")
+                    loginViewModel.loginResult = .loading
+                    print("Signed Out")
                 } label: {
                     Text("Sign Out")
                         .foregroundColor(Color.white)
@@ -95,39 +96,6 @@ struct SettingView: View {
     
 
 
-//VStack(alignment: .leading) {
-//
-//    Image(systemName: "person.crop.circle.fill")
-//        .resizable()
-//        .frame(width:50 ,height: 50)
-//        .padding(.bottom)
-//
-//    Text("Tedtya RADY")
-//
-//    Spacer()
-//
-//
-//    List {
-//        ScrollView {
-//            VStack(alignment: .leading, spacing: 30) {
-//                ForEach(allViews) { each in
-//                    NavigationLink {
-//                        AnyView(each.view)
-//                    } label: {
-//                        Text(each.rawValue.capitalized)
-//                            .font(.system(size: 18))
-//                            .foregroundColor(Color.black)
-//
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    .background(Color.white)
-//    .padding()
-//    .padding()
-//}
 
 
 
@@ -143,114 +111,3 @@ struct SettingView: View {
 
 
 
-
-
-
-// MARK: - explanation of delegate and referencing
-
-/*
- protocol ChildDelegate: AnyObject {
- func onTrigger()
- }
- 
- class Parent: ChildDelegate  {
- 
- let child: Child = .init()
- 
- init() {
- child.delegate = self
- }
- 
- func onTrigger() {
- print("Hello Trigger")
- }
- }
- 
- 
- class Child {
- 
- weak var delegate: ChildDelegate?
- 
- init() {
- }
- 
- func test() {
- delegate?.onTrigger()
- }
- 
- }
- 
- 
- 
- 
- class Vehicle {
- 
- let motor: Motor = .init()
- 
- init () {
- }
- 
- func horn() {
- print("Honk Honk")
- }
- }
- 
- 
- class Motor {
- weak var delegate: MotorDelegate?
- 
- init() {
- }
- 
- func startHorn() {
- delegate?.horn()
- }
- }
- 
- protocol MotorDelegate:AnyObject {
- func horn()
- }
- 
- 
- 
- 
- 
- 
- 
- class ParentTwo  {
- 
- let child: ChildTwo = .init()
- 
- init() {
- child.onCompletion = {
- self.onTrigger()
- }
- 
- child.test()
- }
- 
- func onTrigger() {
- print("Hello Trigger")
- }
- 
- }
- 
- 
- 
- class ChildTwo {
- 
- var onCompletion: (() -> Void)? = nil
- 
- init() {
- 
- }
- 
- func test() {
- self.onCompletion?()
- //        delegate?.onTrigger()
- }
- 
- }
- 
- 
- */
