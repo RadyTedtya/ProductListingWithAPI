461c941964c6317995d2558a5d2ef3331a1f6b77
tedtya rady <tedtyarady@gmail.com>
2023-03-12 15:24:03 +1100

    Add a singleton file

---
+++ ToDoList/Model/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tedtya rady on 12/3/2023.
+//
+
+import Foundation
+
+class ToDoListApp {
+    
+    var loginResult: Bool = false
+    
+    static let shared = ToDoList()
+    
+    private init() { }
+}
--- ToDoList/ToDoList.swift
+++ ToDoList/ToDoList.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct ToDoListApp: App {
+struct ToDoList: App {
     var body: some Scene {
         WindowGroup {
             MainView()
